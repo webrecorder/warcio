ff079b2be1f11074ae319ada6fcf8378541499f3
Greg Lindahl <lindahl@pbm.com>
2017-05-11 20:35:29 -0700

    pytest pickyness

--- test/test_utils.py
+++ test/test_utils.py
@@ -10,30 +10,31 @@ except ImportError:
     pass
 
 
-def test_headers_to_str_headers():
-    result = [('foo', 'bar'), ('baz', 'barf')]
+class TestUtils(object):
+    def test_headers_to_str_headers(self):
+        result = [('foo', 'bar'), ('baz', 'barf')]
 
-    header_dict = {'foo': b'bar', b'baz': 'barf'}
-    ret = utils.headers_to_str_headers(header_dict)
-    assert Counter(ret) == Counter(result)
+        header_dict = {'foo': b'bar', b'baz': 'barf'}
+        ret = utils.headers_to_str_headers(header_dict)
+        assert Counter(ret) == Counter(result)
 
-    aiohttp_raw_headers = ((b'foo', b'bar'), (b'baz', b'barf'))
-    assert Counter(utils.headers_to_str_headers(aiohttp_raw_headers)) == Counter(result)
+        aiohttp_raw_headers = ((b'foo', b'bar'), (b'baz', b'barf'))
+        assert Counter(utils.headers_to_str_headers(aiohttp_raw_headers)) == Counter(result)
 
-@pytest.mark.skipif('multidict' not in sys.modules, reason='requires multidict be installed')
-def test_multidict_headers_to_str_headers():
-    result = [('foo', 'bar'), ('baz', 'barf')]
+    @pytest.mark.skipif('multidict' not in sys.modules, reason='requires multidict be installed')
+    def test_multidict_headers_to_str_headers(self):
+        result = [('foo', 'bar'), ('baz', 'barf')]
 
-    aiohttp_headers = MultiDict(foo='bar', baz=b'barf')
-    ret = utils.headers_to_str_headers(aiohttp_headers)
-    assert Counter(ret) == Counter(result)
+        aiohttp_headers = MultiDict(foo='bar', baz=b'barf')
+        ret = utils.headers_to_str_headers(aiohttp_headers)
+        assert Counter(ret) == Counter(result)
 
-    # This case-insensitive thingie titlecases the key
-    aiohttp_headers = CIMultiDict(foo='bar', baz=b'barf')
-    titlecase_result = [('Foo', 'bar'), ('Baz', 'barf')]
-    rtitlecase_result = titlecase_result
-    rtitlecase_result.reverse()
+        # This case-insensitive thingie titlecases the key
+        aiohttp_headers = CIMultiDict(foo='bar', baz=b'barf')
+        titlecase_result = [('Foo', 'bar'), ('Baz', 'barf')]
+        rtitlecase_result = titlecase_result
+        rtitlecase_result.reverse()
 
-    ret = utils.headers_to_str_headers(aiohttp_headers)
-    assert Counter(ret) == Counter(titlecase_result)
+        ret = utils.headers_to_str_headers(aiohttp_headers)
+        assert Counter(ret) == Counter(titlecase_result)
 
